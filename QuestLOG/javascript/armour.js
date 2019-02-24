499212d204f518b03e0e65bd495f0b0001746b58
mharquail <43412943+mharquail@users.noreply.github.com>
2019-02-23 23:53:46 -0500

    Updated armour

--- QuestLOG/javascript/armour.js
+++ QuestLOG/javascript/armour.js
@@ -2,6 +2,7 @@
 class armour{
 	constructor(){
 		var name;
+		var material;
 		var type;
 		var rarity;
 		var defense;
@@ -13,6 +14,14 @@ class armour{
 
 	setName(val){
 		this.name = val;
+	}
+
+	getMaterial(){
+		return this.material;
+	}
+
+	setMaterial(val){
+		this.material = val;
 	}
 
 	getType(){
@@ -60,13 +69,93 @@ class armour{
 	}
 
 	determineType(){
+		var typeRoll = Math.floor((Math.random() * 5) + 1);
 
+		switch(t){
+			case 1:
+				determineHelmet();
+				break;
+			case 2:
+				determineChest();
+				break;
+			case 3:
+				determineGloves();
+				break;
+			case 4:
+				determinePants();
+				break;
+			case 5:
+				determineShoes();
+				break;
+		}
+	}
+
+	determineHelmet(){
+		var nameRoll = 
+	}
+	
+	determineChest(){
+
+	}
+	
+	determineGloves(){
+
+	}
+	
+	determinePants(){
+
+	}
+	
+	determineShoes(){
+
+	}
+	
+	determineMaterial(){
+		var typeRoll = Math.floor((Math.random() * 10) + 1);
+
+		switch(t){
+			case 1:
+				nameHold = "Helmet";
+				break;
+			case 2:
+				nameHold = "Shirt";
+				break;
+			case 3:
+				nameHold = "Sword";
+				break;
+			case 4:
+				nameHold = "Knife";
+				break;
+			case 5:
+				nameHold = "Sabre";
+				break;
+			case 6:
+				nameHold = "Rapier";
+				break;
+			case 7:
+				nameHold = "Scimitar";
+				break;
+			case 8:
+				nameHold = "Gladius";
+				break;
+			case 9:
+				nameHold = "Katana";
+				break;
+			case 10:
+				nameHold = "Claymore";
+				break;
+		}
 	}
 
 	generateArmourName(){
 		var str;
 		var r = getRarity();
 		var t = getType();
+
+
 	}
 }
 
+function randomArmour(){
+
+}
